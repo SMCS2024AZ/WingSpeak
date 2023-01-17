e2d072c3452f5a2bf941033975f86068e2bd944c
SMCS2024AZ <102745665+SMCS2024AZ@users.noreply.github.com>
2023-01-17 14:21:05 -0500

    Delete public directory

--- public/scripts/userLogin.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $("#inputName").submit((e) => {
-    if ($("#name").val() == null) {
-      alert("Please choose a name.")
-      e.preventDefault()
-    }
-  })
-})
